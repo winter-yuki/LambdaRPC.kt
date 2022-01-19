29b3bbbb023d42900bfc46f787094fbe4f33da87
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2022-01-19 20:03:15 +0300

    Extract AbstractChannelFunction

--- src/main/kotlin/io/lambdarpc/functions/frontend/ChannelFunction.kt
+++ src/main/kotlin/io/lambdarpc/functions/frontend/ChannelFunction.kt
@@ -1,33 +1,40 @@
 package io.lambdarpc.functions.frontend
 
 import io.lambdarpc.exceptions.UnknownMessageType
-import io.lambdarpc.serialization.ChannelRegistry
-import io.lambdarpc.serialization.FunctionRegistry
-import io.lambdarpc.serialization.Serializer
-import io.lambdarpc.serialization.scope
-import io.lambdarpc.transport.grpc.InMessage
+import io.lambdarpc.serialization.*
+import io.lambdarpc.transport.grpc.Entity
 import io.lambdarpc.transport.grpc.executeRequest
 import io.lambdarpc.utils.AccessName
 
-class ChannelFunction1<A, R>(
+abstract class AbstractChannelFunction<R>(
     val name: AccessName,
-    private val channelRegistry: ChannelRegistry<InMessage>,
-    val s1: Serializer<A>,
-    val rs: Serializer<R>
-) : suspend (A) -> R {
-    override suspend fun invoke(arg: A): R = scope(FunctionRegistry(), channelRegistry) {
+    protected val channelRegistry: ChannelRegistry,
+    private val rs: Serializer<R>
+) {
+    protected suspend operator fun SerializationScope.invoke(vararg entities: Entity): R {
         val executeRequest = executeRequest {
             accessName = name.n
-            args.add(s1.encode(arg))
+            entities.forEach { args.add(it) }
         }
-        val response = channelRegistry {
-            send(executeRequest)
-            receive()
+        val response = channelRegistry.use { channel ->
+            channel.send(executeRequest)
+            channel.receive()
         }
-        when {
+        return when {
             response.hasResult() -> rs.decode(response.result)
             response.hasError() -> TODO("Error handling")
             else -> throw UnknownMessageType("execute result")
         }
+    }
+}
+
+class ChannelFunction1<A, R>(
+    name: AccessName,
+    channelRegistry: ChannelRegistry,
+    private val s1: Serializer<A>,
+    rs: Serializer<R>
+) : AbstractChannelFunction<R>(name, channelRegistry, rs), suspend (A) -> R {
+    override suspend fun invoke(arg: A): R = scope(FunctionRegistry(), channelRegistry) {
+        invoke(s1.encode(arg))
     }
 }
