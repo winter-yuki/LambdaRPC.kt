dcfc8440eb5d098c8fbce9864d0c3dd56e687a3e
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2022-02-08 17:48:46 +0300

    Add gradle modules

---
+++ examples/src/main/kotlin/basic/service1/Facade.kt
@@ -0,0 +1,30 @@
+@file:Suppress("PackageDirectoryMismatch")
+
+package io.lambdarpc.examples.basic.service1.facade
+
+import basic.Point
+import basic.service1.NumpyArrayIntCoder
+import basic.serviceId1
+import io.lambdarpc.dsl.*
+
+val conf = Configuration(
+    serviceId = serviceId1
+)
+
+val add5 by conf.def<Int, Int>()
+val eval5 by conf.def(f1<Int, Int>(), d<Int>())
+val specializeAdd by conf.def(d<Int>(), f1<Int, Int>())
+val executeAndAdd by conf.def(f1<Int, Int>(), f1<Int, Int>())
+val distance by conf.def<Point, Point, Double>()
+val normFilter by conf.def(
+    d<List<Point>>(),
+    f2(d<Point>(), f1<Point, Double>(), d<Boolean>()),
+    d<List<Point>>()
+)
+val mapPoints by conf.def(d<List<Point>>(), f1<Point, Double>(), d<List<Double>>())
+val normMap by conf.def(
+    d<List<Point>>(),
+    f1(f1<Point, Double>(), f1<Point, Double>()),
+    d<List<Double>>()
+)
+val numpyAdd by conf.def(d<Int>(), NumpyArrayIntCoder, NumpyArrayIntCoder)
---
+++ examples/src/main/kotlin/basic/stress/Main.kt
@@ -0,0 +1,46 @@
+package basic.stress
+
+import basic.Point
+import io.lambdarpc.dsl.cf
+import io.lambdarpc.dsl.serviceDispatcher
+import io.lambdarpc.examples.basic.service1.facade.*
+import io.lambdarpc.examples.basic.service2.facade.norm1
+import io.lambdarpc.examples.basic.service2.facade.norm2
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+
+val serviceDispatcher = serviceDispatcher(
+    basic.serviceId1 to basic.endpoint1,
+    basic.serviceId2 to basic.endpoint2
+)
+
+@OptIn(DelicateCoroutinesApi::class)
+fun main(): Unit = runBlocking(serviceDispatcher + newSingleThreadContext("name")) {
+    repeat(100) {
+        launch {
+            println("add5(2) = ${add5(2)}")
+        }
+        launch {
+            val m = 3
+            println("eval5 { it + m } = ${eval5 { it + m }}")
+        }
+        launch {
+            println("specializeAdd(5)(37) = ${specializeAdd(5)(37)}")
+        }
+        launch {
+            println("executeAndAdd { it + 12 }(100) = ${executeAndAdd { it + 12 }(100)}")
+        }
+        val ps = listOf(Point(0.0, 0.0), Point(2.0, 1.0), Point(1.0, 1.5))
+        launch {
+            println("normFilter($ps) { p, norm -> 2 <= norm(p) } = ${normFilter(ps) { p, norm -> 2 <= norm(p) }}")
+        }
+        launch {
+            println("mapPoints(ps, norm()) = ${mapPoints(ps, norm1())}")
+        }
+        launch {
+            println("mapPoints(ps, norm()) = ${mapPoints(ps, cf(norm2))}")
+        }
+    }
+}
