ad6eab626bd5b13075472e2cda8ac9108b516aa5
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2022-02-19 17:35:37 +0300

    Add channel functions

--- lambdarpc/src/main/kotlin/io/lambdarpc/functions/frontend/ChannelFunction.kt
+++ lambdarpc/src/main/kotlin/io/lambdarpc/functions/frontend/ChannelFunction.kt
@@ -1,7 +1,73 @@
 package io.lambdarpc.functions.frontend
 
+import io.lambdarpc.coders.CodingContext
+import io.lambdarpc.coders.Decoder
+import io.lambdarpc.coders.Encoder
+import io.lambdarpc.coders.withContext
+import io.lambdarpc.transport.grpc.Entity
 import io.lambdarpc.utils.AccessName
 
+/**
+ * [ChannelFunction] is a [FrontendFunction] that uses existing bidirectional connection
+ * to communication with its backend part.
+ */
 internal interface ChannelFunction : FrontendFunction {
     val accessName: AccessName
+}
+
+internal abstract class AbstractChannelFunction(
+    private val executionChannel: ExecutionChannel
+) : ChannelFunction {
+    protected suspend fun invoke(vararg entities: Entity): Entity =
+        executionChannel.execute(accessName, entities.toList())
+}
+
+internal class ChannelFunction0<R>(
+    override val accessName: AccessName,
+    executionChannel: ExecutionChannel,
+    private val context: CodingContext,
+    private val rc: Decoder<R>
+) : AbstractChannelFunction(executionChannel), suspend () -> R {
+    override suspend fun invoke(): R = withContext(context) {
+        rc.decode(super.invoke())
+    }
+}
+
+internal class ChannelFunction1<A, R>(
+    override val accessName: AccessName,
+    executionChannel: ExecutionChannel,
+    private val context: CodingContext,
+    private val c1: Encoder<A>,
+    private val rc: Decoder<R>
+) : AbstractChannelFunction(executionChannel), suspend (A) -> R {
+    override suspend fun invoke(a1: A): R = withContext(context) {
+        rc.decode(invoke(c1.encode(a1)))
+    }
+}
+
+internal class ChannelFunction2<A, B, R>(
+    override val accessName: AccessName,
+    executionChannel: ExecutionChannel,
+    private val context: CodingContext,
+    private val c1: Encoder<A>,
+    private val c2: Encoder<B>,
+    private val rc: Decoder<R>
+) : AbstractChannelFunction(executionChannel), suspend (A, B) -> R {
+    override suspend fun invoke(a1: A, a2: B): R = withContext(context) {
+        rc.decode(invoke(c1.encode(a1), c2.encode(a2)))
+    }
+}
+
+internal class ChannelFunction3<A, B, C, R>(
+    override val accessName: AccessName,
+    executionChannel: ExecutionChannel,
+    private val context: CodingContext,
+    private val c1: Encoder<A>,
+    private val c2: Encoder<B>,
+    private val c3: Encoder<C>,
+    private val rc: Decoder<R>
+) : AbstractChannelFunction(executionChannel), suspend (A, B, C) -> R {
+    override suspend fun invoke(a1: A, a2: B, a3: C): R = withContext(context) {
+        rc.decode(invoke(c1.encode(a1), c2.encode(a2), c3.encode(a3)))
+    }
 }
--- lambdarpc/src/main/kotlin/io/lambdarpc/functions/frontend/ChannelRegistry.kt
+++ lambdarpc/src/main/kotlin/io/lambdarpc/functions/frontend/ChannelRegistry.kt
@@ -1,51 +1,86 @@
 package io.lambdarpc.functions.frontend
 
 import io.lambdarpc.exceptions.LambdaRpcException
-
-class CallDisconnectedChannelFunction :
-    LambdaRpcException(
-        "Unable to call invalidated ChannelFunction. " +
-                "Convert it to the ClientFunction before saving"
-    )
+import io.lambdarpc.transport.grpc.Entity
+import io.lambdarpc.transport.grpc.ExecuteRequest
+import io.lambdarpc.transport.serialization.ExecuteRequest
+import io.lambdarpc.utils.AccessName
+import io.lambdarpc.utils.ExecutionId
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.flow.MutableSharedFlow
+import java.io.Closeable
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 
 /**
- * Controls offered channel lifetime
+ * Is thrown when [ChannelFunction] is called after its connection is already closed.
  */
-internal interface ExecutionChannel
+class CallDisconnectedChannelFunction :
+    LambdaRpcException("Unable to call invalidated ChannelFunction.")
 
-internal class ChannelRegistry : ExecutionChannel {
-//    private val requests: MutableMap<HeadExecutionId, MutableSharedFlow<ExecuteRequest>> = ConcurrentHashMap()
-//    private val completable: MutableMap<ExecutionId, CompletableDeferred<Entity>> = ConcurrentHashMap()
-//
-//    fun open(headExecutionId: HeadExecutionId, flow: MutableSharedFlow<ExecuteRequest>) {
-//        requests[headExecutionId] = flow
-//    }
-//
-//    fun close(headExecutionId: HeadExecutionId) {
-//        requests.remove(headExecutionId)
-//    }
-//
-//    fun complete(executionId: ExecutionId, entity: Entity) {
-//        completable[executionId]?.complete(entity) ?: error("Nobody waits for the result")
-//    }
-//
-//    fun completeExceptionally(executionId: ExecutionId, throwable: Throwable) {
-//        completable[executionId]?.completeExceptionally(throwable) ?: error("Nobody waits for the result")
-//    }
-//
-//    override suspend fun request(
-//        accessName: AccessName,
-//        headExecutionId: HeadExecutionId,
-//        entities: Iterable<Entity>
-//    ): Entity {
-//        val id = ExecutionId.random()
-//        completable[id] = CompletableDeferred()
-//        return try {
-//            val request = ExecuteRequest(accessName, id, entities)
-//            requests[headExecutionId]?.emit(request) ?: throw CallDisconnectedChannelFunction()
-//            completable[id]?.await() ?: error("$id does not exist in channel registry")
-//        } finally {
-//            completable.remove(id)
-//        }
-//    }
+/**
+ * Allows [ChannelFunction] to communicate with its backend function.
+ */
+internal fun interface ExecutionChannel {
+    suspend fun execute(
+        accessName: AccessName,
+        entities: Iterable<Entity>
+    ): Entity
+}
+
+@Suppress("unused")
+@JvmInline
+internal value class ExecutionChannelId private constructor(private val id: UUID) {
+    companion object {
+        fun random() = ExecutionChannelId(UUID.randomUUID())
+    }
+}
+
+/**
+ * Keeps all execution channels for all functions to be possibly reopened later.
+ */
+internal class ChannelRegistry {
+    private val channels: MutableMap<ExecutionChannelId, MutableSharedFlow<ExecuteRequest>> = ConcurrentHashMap()
+
+    inline fun <R> useController(
+        executeRequests: MutableSharedFlow<ExecuteRequest>,
+        block: (ExecutionChannelController) -> R
+    ): R = ExecutionChannelController(executeRequests).use(block)
+
+    /**
+     * Allows creating [ExecutionChannel] and controls its lifetime.
+     */
+    inner class ExecutionChannelController(executeRequests: MutableSharedFlow<ExecuteRequest>) : Closeable {
+        private val channelId = ExecutionChannelId.random()
+        private val completableResults: MutableMap<ExecutionId, CompletableDeferred<Entity>> = ConcurrentHashMap()
+
+        init {
+            channels[channelId] = executeRequests
+        }
+
+        fun complete(executionId: ExecutionId, entity: Entity) {
+            completableResults[executionId]?.complete(entity) ?: error("Nobody waits for the result")
+        }
+
+        fun completeExceptionally(executionId: ExecutionId, throwable: Throwable) {
+            completableResults[executionId]?.completeExceptionally(throwable) ?: error("Nobody waits for the result")
+        }
+
+        fun createChannel() = ExecutionChannel { accessName, entities ->
+            val id = ExecutionId.random()
+            val deferred = CompletableDeferred<Entity>()
+            completableResults[id] = deferred
+            try {
+                val request = ExecuteRequest(accessName, id, entities)
+                channels[channelId]?.emit(request) ?: throw CallDisconnectedChannelFunction()
+                deferred.await()
+            } finally {
+                completableResults.remove(id)
+            }
+        }
+
+        override fun close() {
+            channels.remove(channelId)
+        }
+    }
 }
